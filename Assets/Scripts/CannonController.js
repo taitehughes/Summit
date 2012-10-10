0b5441579795d154bd04720ac5a707d48735ed8c
Austin Blakeslee <atb046@gmail.com>
2012-10-10 15:31:10 -0400

    Readd Cannon
    Cannon prefab made that fires cannonballs

---
+++ Assets/Scripts/CannonController.js
@@ -0,0 +1,17 @@
+#pragma strict
+var cb : GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.LeftShift)) {
+		Fire();
+	}
+}
+
+function Fire() {
+	var cannonball = Instantiate(cb, Vector3(gameObject.transform.position.x+1,gameObject.transform.position.y,gameObject.transform.position.z),Quaternion.identity);
+	cannonball.rigidbody.velocity.x= 50;
+}
